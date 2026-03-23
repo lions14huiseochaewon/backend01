cfd2b03a4651044fb9b10544b9095974c25bbe32
chae_one <chaewonp2005@gmail.com>
2026-03-24 00:44:39 +0900

    CRUD진짜초안

--- deskresearch/views.py
+++ deskresearch/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
 
+from .forms import PostForm
+
 def home(request):
     posts = Post.objects.order_by('-created_at')
     return render(request,'home.html',{'Posts':posts})
 
 def new(request):
-    return render(request, 'new.html')
+    form=PostForm()
+    return render(request, 'new.html', {'form':form})
 
 def create(request):
-    new_deskresearch=Post()
-    new_deskresearch.title=request.POST['title']
-    new_deskresearch.content=request.POST['content']
-    new_deskresearch.save()
+    form=PostForm(request.POST,request.FILES)
+    if form.is_valid():
+        new_deskresearch=form.save(commit=False)
+        new_deskresearch.save()
+        return redirect('deskresearch:detail',new_deskresearch.id)
     return redirect('deskresearch:home') #여기도 deskresearch:home 해줘야함
 
 def detail(request,post_id):
