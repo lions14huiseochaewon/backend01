d015d88bd8e69f949a39cb7e82c98af2cbd8108d
chae_one <chaewonp2005@gmail.com>
2026-03-24 00:11:00 +0900

    CRUD초안

---
+++ deskresearch/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Post
+
+def home(request):
+    posts = Post.objects.order_by('-created_at')
+    return render(request,'home.html',{'Posts':posts})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_deskresearch=Post()
+    new_deskresearch.title=request.POST['title']
+    new_deskresearch.content=request.POST['content']
+    new_deskresearch.save()
+    return redirect('deskresearch:home') #여기도 deskresearch:home 해줘야함
+
+def detail(request,post_id):
+    post_detail=get_object_or_404(Post, pk=post_id)
+    return render(request,'detail.html',{'post':post_detail})
+
+def delete(request, post_id):
+    delete_deskresearch = get_object_or_404(Post, pk=post_id)
+    delete_deskresearch.delete()
+    return redirect('deskresearch:home')
+
+def update_page(request, post_id):
+    update_deskresearch=get_object_or_404(Post,pk=post_id)
+    return render(request,'update.html',{'update_deskresearch':update_deskresearch})
+
+def update_post(request, post_id):
+    update_deskresearch=get_object_or_404(Post,pk=post_id)
+    update_deskresearch.title=request.POST['title']
+    update_deskresearch.content=request.POST['content']
+    update_deskresearch.save()
+    return redirect('deskresearch:home')
